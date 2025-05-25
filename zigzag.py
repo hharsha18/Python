4b6df8e740da077fe40fddae429af2e57c124998
hharsha18 <hhharsha2006@gmail.com>
2025-05-25 16:56:53 +0530

    Create zigzag.py

---
+++ zigzag.py
@@ -0,0 +1,9 @@
+print("Harsha D S,USN:1AY24AI041,SEC:M")
+lines = 9
+max_spaces = lines 
+for i in range(lines):
+    if i <= max_spaces:
+        spaces = i
+    else:
+        spaces = lines - i - 1
+    print(" " * spaces + "*" * 8)
