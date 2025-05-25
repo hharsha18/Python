49f4aa81301d996ed5a8b53ea075035df24e6f83
hharsha18 <hhharsha2006@gmail.com>
2025-05-25 17:26:49 +0530

    Create fantasygameinvetor.py

---
+++ fantasygameinvetor.py
@@ -0,0 +1,17 @@
+print("Harsha D S,USN:1AY24AI041,SEC:M")
+game_inventory = {'rope' : 1, 'gold coin' : 42}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+def addToInventory(inventory, addedItems):
+	print('Inventory:')
+	total = 0
+	for k, v in inventory.items():
+		if k in addedItems:
+			o=addedItems.count(k)
+			inventory[k]=v+o
+	for k2 in addedItems:
+		if k2 not in inventory.keys():
+			inventory[k2]=addedItems.count(k2)
+	for k3, v2 in inventory.items():
+		print(v2, k3)
+		total += v2
+	print('The total no of items is: ' + str(total))
