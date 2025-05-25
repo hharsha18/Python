a5f0b9befd78d3419941c88c1c7d543327c5cc71
hharsha18 <hhharsha2006@gmail.com>
2025-05-25 17:04:44 +0530

    Create commacode.py

---
+++ commacode.py
@@ -0,0 +1,12 @@
+print("Harsha D S,USN:1AY24AI041,SEC:M")
+def list_to_string(items):
+    if not items:
+        return ''
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f'{items[0]} and {items[1]}'
+    else:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+spam = ['apples', 'bananas', 'tofu', 'cats']
+print(list_to_string(spam))
