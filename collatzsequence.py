fc45c08fdfbea454712f7e4f65277ea3015d1a55
hharsha18 <hhharsha2006@gmail.com>
2025-05-25 17:06:20 +0530

    Create collatzsequence.py

---
+++ collatzsequence.py
@@ -0,0 +1,14 @@
+print("Harsha D S,USN:1AY24AI041,SEC:M")
+def collatz(number):
+    if number % 2 == 0:
+        result = number 
+    else:
+        result = 3 * number + 1
+    print(result)
+    return result
+try:
+    user_input = int(input("Enter an integer: "))
+    while user_input != 1:
+        user_input = collatz(user_input)
+except ValueError:
+    print("Please enter a valid integer.")
