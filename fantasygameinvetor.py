d9ca12c58f346ac1dcd97b1034511743b8d8bd9d
hharsha18 <hhharsha2006@gmail.com>
2025-05-26 19:17:38 +0530

    Update fantasygameinvetor.py
    Signed-off-by: hharsha18 <hhharsha2006@gmail.com>

--- fantasygameinvetor.py
+++ fantasygameinvetor.py
@@ -1,17 +1,22 @@
-print("Harsha D S,USN:1AY24AI041,SEC:M")
-game_inventory = {'rope' : 1, 'gold coin' : 42}
+print("Harsha D S, USN: 1AY24AI041, SEC: M")
+
+game_inventory = {'rope': 1, 'gold coin': 42}
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
 def addToInventory(inventory, addedItems):
-	print('Inventory:')
-	total = 0
-	for k, v in inventory.items():
-		if k in addedItems:
-			o=addedItems.count(k)
-			inventory[k]=v+o
-	for k2 in addedItems:
-		if k2 not in inventory.keys():
-			inventory[k2]=addedItems.count(k2)
-	for k3, v2 in inventory.items():
-		print(v2, k3)
-		total += v2
-	print('The total no of items is: ' + str(total))
+    print("\nUpdated Inventory:")
+    total = 0
+
+    for item in addedItems:
+        inventory[item] = inventory.get(item, 0) + 1
+
+    print(f"{'Item Name':<15}{'Count':>10}")
+    print("-" * 25)
+    for item, count in inventory.items():
+        print(f"{item:<15}{count:>10}")
+        total += count
+
+    print("-" * 25)
+    print(f"{'Total number of items:':<15}{total:>10}")
+
+addToInventory(game_inventory, dragon_loot)
