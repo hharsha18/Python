f173d3c40b4840e8ac8ee0976b924256e4f3877e
hharsha18 <hhharsha2006@gmail.com>
2025-05-26 18:49:20 +0530

    Update charactergrid.py
    Signed-off-by: hharsha18 <hhharsha2006@gmail.com>

--- charactergrid.py
+++ charactergrid.py
@@ -1,15 +1,16 @@
-print("Harsha D S,USN:1AY24AI041,SEC:M")
+print("Harsha D S, USN: 1AY24AI041, SEC: M")
+
 grid = [['.', '.', '.', '.', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['.', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', 'O', 'O', '.', '.'],     
+        ['.', 'O', 'O', 'O', 'O', '.'],    
+        ['O', 'O', 'O', 'O', '.', '.'],       
+        ['.', 'O', 'O', 'O', 'O', '.'],      
+        ['.', 'O', 'O', 'O', 'O', 'O'],    
+        ['O', 'O', 'O', 'O', '.', '.'],      
+        ['.', 'O', 'O', '.', '.', '.'],       
         ['.', '.', '.', '.', '.', '.']]
 
-for y in range(len(grid[0])):
-    for x in range(len(grid)):
-        print(grid[x][y], end='')
-    print()
+for y in range(len(grid[0])):        
+    for x in range(len(grid)):       
+        print(grid[x][y], end='')     
+    print()
