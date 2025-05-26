edc23be3e848f5ec2a47b738f0929654439f0130
hharsha18 <hhharsha2006@gmail.com>
2025-05-26 19:32:40 +0530

    Update table printer.py
    Signed-off-by: hharsha18 <hhharsha2006@gmail.com>

--- table printer.py
+++ table printer.py
@@ -8,9 +8,10 @@ def print_table(data):
     num_columns = len(data[0])
     col_widths = [max(len(str(row[i])) for row in data) for i in range(num_columns)]
 
+    
     header = data[0]
     print(" | ".join(str(header[i]).ljust(col_widths[i]) for i in range(num_columns)))
-    
+
     print("-+-".join('-' * col_widths[i] for i in range(num_columns)))
 
     for row in data[1:]:
@@ -22,3 +23,6 @@ table_data = [
     ["Sneha", 22, "Mumbai"],
     ["Aditya", 31, "Chennai"]
 ]
+
+
+print_table(table_dat
